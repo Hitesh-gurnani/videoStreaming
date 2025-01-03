3b50d7734166835c2e7d27ccf381922a99cbe281
Hitesh-Gurnani <newidhiteshgurnani@gmail.com>
2025-01-03 13:12:22 +0530

    intial-commit

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const server_config_1 = require("./config/server.config");
+const routes_1 = __importDefault(require("./routes"));
+const app = (0, express_1.default)();
+app.use("/api", routes_1.default);
+app.listen(server_config_1.PORT, () => {
+    console.log("Server is running on http://localhost:3000");
+});
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express, { Express } from "express";
+import { PORT } from "./config/server.config";
+import apiRouter from "./routes";
+const app: Express = express();
+
+app.use("/api", apiRouter);
+
+app.listen(PORT, () => {
+  console.log("Server is running on http://localhost:3000");
+});
